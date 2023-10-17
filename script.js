a901a5e94ea20b98641a908dffd7636876983b3e
Loloko <112960727+Loloko1234@users.noreply.github.com>
2023-10-17 21:55:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,117 @@ function decrease() {
     inputElement.value = value;
   }
 }
+function changeText(index) {
+  const content = document.getElementById("content");
+  const texts = [
+    '<span class="content--footer">SPC Gear GK630K Onyx White Tournament Kailh Brown RGB to biała odsłona klawiatury GK630K. Jej jasna stylistyka nawiązuje do wyjątkowo rzadkiej białej odmiany onyksu — cennego minerału znanego z wysokiej wytrzymałości i nietuzinkowego wyglądu. Mała, skrócona o blok numeryczny konstrukcja świetnie sprawdza się zarówno w domowym zaciszu, jak również na turniejach czy LAN party ze znajomymi. Klawiatura wyposażona została w wielokolorowe podświetlenie RGB, które wraz z nakładkami typu Pudding rozświetlą stanowisko gracza pełną paletą barw. Przełączniki Kailh Brown dzięki uniwersalnej i zbalansowanej charakterystyce pozwalają na grę na najwyższym poziomie oraz wygodne i precyzyjne pisanie.</span>',
+  ];
+
+  content.innerHTML = texts[index];
+}
+
+function displayTable() {
+  const content = document.getElementById("content");
+
+  const tableContent = `
+  <div class="table-container">
+  <div class="table-column">
+      <ul class="table-wrapper">
+        <li>
+          <span>Kod produktu</span>
+          <span>SPG063</span>
+        </li>
+        <li>
+          <span>EAN</span>
+          <span>5903018661865</span>
+        </li>
+        <li>
+          <span>Wymiary produktu [MM]</span>
+          <span>361,5×140×42,5</span>
+        </li>
+        <li>
+          <span>Waga netto [G]</span>
+          <span>915</span>
+        </li>
+        <li>
+          <span>Waga brutto [G]</span>
+          <span>1105</span>
+        </li>
+        <li>
+          <span>Wymiary opakowania [MM]</span>
+          <span>400×190×50</span>
+        </li>
+        <li>
+          <span>Typ klawiatury</span>
+          <span>Mechaniczna</span>
+        </li>
+        <li>
+          <span>Liczba przycisków</span>
+          <span>87</span>
+        </li>
+        <li>
+          <span>Materiał nakładek</span>
+          <span>PBT (PUDDING WHITE)</span>
+        </li>
+        <li>
+          <span>Rodzaj przełączników</span>
+          <span>KAILH</span>
+        </li>
+        <li>
+          <span>Kolor przełączników</span>
+          <span>BROWN/BRĄZOWE</span>
+        </li>
+    </ul>
+    </div>
+
+        <div class="table-column">
+        <ul class="table-wrapper">
+
+        
+        <li>
+          <span>Częstotliwość próbkowania</span>
+          <span>1000</span>
+        </li>
+        <li>
+          <span>Rodzaj podświetlenia</span>
+          <span>RGB</span>
+        </li>
+        <li>
+          <span>Długość kabla [M]</span>
+          <span>1,8</span>
+        </li>
+        <li>
+          <span>Rodzaj wtyku</span>
+          <span>POZŁACANY USB</span>
+        </li>
+        <li>
+          <span>Programowalne makra</span>
+          <span>TAK</span>
+        </li>
+        <li>
+          <span>N-Key rollover</span>
+          <span>PEŁNY</span>
+        </li>
+        <li>
+          <span>Anti-Ghosting</span>
+          <span>PEŁNY</span>
+        </li>
+        <li>
+          <span>Kompatybilność</span>
+          <span>WINDOWS, MAC, LINUX</span>
+        </li>
+        <li>
+          <span>Oprogramowanie</span>
+          <span>TAK (OPCJONALNE DLA WINDOWS 10)</span>
+        </li>
+        <li>
+          <span>Gwarancja (miesiące)</span>
+          <span>24</span>
+        </li>
+      </ul>
+      </div>
+      </div>
+    `;
+
+  content.innerHTML = tableContent;
+}
